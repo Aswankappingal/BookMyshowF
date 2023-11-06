31a709049117dcc97826e208e20dd7aa82395dac
Aswankappingal <instamagic774@gmail.com>
2023-11-06 21:27:48 +0530

    file is added

---
+++ front_end/js/created.js
@@ -0,0 +1,30 @@
+
+document.getElementById("submit-btn").addEventListener("click",(e)=>{
+    e.preventDefault();
+
+    let user=document.getElementById("user-name").value
+    let password=document.getElementById("pass-word").value
+
+
+
+    fetch("http://localhost:3005/BookMyShow/addUser",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({
+            user,password
+        })
+
+    })
+    .then((res)=>{
+        console.log(res.status);
+        if(res.status==201){
+            alert("Registartion sucessfull")
+            
+        }
+        else{
+            alert("Registartion Failed")
+        }
+    })
+    .catch((error)=>{alert("server not connected")})
+
+})
